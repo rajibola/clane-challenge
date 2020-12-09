692bc851b3875144040486b406438b0023aa4f03
Ridwan Ajibola <rajibola1997@gmail.com>
2020-12-09 22:37:59 +0100

    feat: created colors file

---
+++ colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  gray: '#EEEDEF',
+  darkGray: '#59688D',
+  lightBlue: '#E5EDFE',
+  darkBlue: '#75829F',
+  secondary: '#F3F2F3',
+  primary: '#F7F8FB',
+  dark: '#636468',
+};
