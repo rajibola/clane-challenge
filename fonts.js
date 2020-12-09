862635926a51a9c3e547210a2b246b47c0005b70
Ridwan Ajibola <rajibola1997@gmail.com>
2020-12-09 22:51:36 +0100

    feat: Created font file

---
+++ fonts.js
@@ -0,0 +1,11 @@
+import {colors} from './colors';
+import {h} from './src/shared/resposive-dimension';
+
+export const fonts = {
+  font16: {
+    textTransform: 'uppercase',
+    fontSize: h(16),
+    lineHeight: h(24),
+    color: colors.dark,
+  },
+};
